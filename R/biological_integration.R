9c87b63a1d757e9afc7a87c5b3beee259ac1b783
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2019-10-22 12:05:34 -0400

    Adding biological integration

---
+++ R/biological_integration.R
@@ -0,0 +1,137 @@
+#' Modules enrichment
+#'
+#' Enrich genes list from modules.
+#'
+#' @param data_expr matrix of expression data with genes as column and samples as row.
+#' @param tom
+#' TODO finish
+#'
+#' @details
+#' TODO
+#' @return
+#' TODO
+#'
+#' @examples
+#' TODO
+#' @import WGCNA
+#' @import gprofiler2
+#'
+#' @export
+
+modules_enrichment <- function(modules, custom_gmt = NULL, ...) {
+  # Checks
+
+  # Splitting genes by module
+  modules_list <- base::split(names(modules), modules)
+
+  # Enrichment
+  enriched_modules <- lapply(modules_list, function(module){
+    gprofiler2::gost(query = module, ...)
+  })
+  # TODO Check why multiple query return less enrichment results than mono query on each module
+
+  return(enriched_modules)
+}
+
+
+#' Modules phenotpic association
+#'
+#' Compute the correlation between all modules and the phenotypic variables
+#'
+#' @param eigengenes Matrix of igengenes provided by the output of modules_detection.
+#' @param phenotypes Matrix of phenotypes for each sample to associate.
+#' TODO finish
+#'
+#' @details
+#' TODO
+#' @return
+#' TODO
+#'
+#' @examples
+#' TODO
+#' @import WGCNA
+#' @import dplyr
+#'
+#' @export
+
+modules_phenotype <- function(eigengenes, phenotypes) {
+  # Checks
+
+  # Design matrix (dummy variable formation for qualitative variables)
+  dummies_var <- lapply(colnames(phenotypes), function(dummy_name) {
+    df <- phenotypes %>% select(dummy_name)
+    if (!is.numeric(df[1,])){
+      model_mat <- model.matrix(
+        formula(paste("~ ", dummy_name, "+ 0")),
+        data = df
+      )
+      colnames(model_mat) <- gsub(dummy_name, "", colnames(model_mat))
+      return(model_mat)
+    } else {
+      return(df)
+    }
+  })
+  design_mat <- as.data.frame(dummies_var, check.names = FALSE)
+
+  # Correlations
+  association <- cor(eigengenes, design_mat)
+
+  # P value associated
+  pval_association <- WGCNA::corPvalueStudent(association, nrow(phenotypes))
+
+  # TODO Check if a correction for multiple testing shouldn't be performed here...
+
+  return(list(
+    association = association %>% as.data.frame,
+    pval = pval_association %>% as.data.frame
+  ))
+}
+
+
+
+#' Heatmap of modules phenotpic association
+#'
+#' Plot a heatmap of the correlation between all modules and the phenotypic variables and the p value associated
+#'
+#' @param modules_phenotype List form modules_phenotype function output
+#' TODO finish
+#'
+#' @details
+#' TODO
+#' @return
+#' TODO
+#'
+#' @examples
+#' TODO
+#' @import ggplot2
+#' @import dplyr
+#' @import tibble
+#' @import tidyr
+#'
+#' @export
+
+plot_modules_phenotype <- function(modules_phenotype, signif_th = 0.05){
+  # Checks
+
+
+  # Data preparation
+  df_cor <- modules_phenotype$association %>%
+    tibble::rownames_to_column(var = "eigengene") %>%
+    tidyr::pivot_longer(-eigengene, names_to = "phenotype", values_to = "cor")
+
+  df_pval <- modules_phenotype$pval %>%
+    tibble::rownames_to_column(var = "eigengene") %>%
+    tidyr::pivot_longer(-eigengene, names_to = "phenotype", values_to = "pval")
+
+  df_total <- bind_cols(df_cor, df_pval %>% select(pval)) %>%
+    mutate(signif = ifelse(pval > signif_th, FALSE, TRUE))
+
+  # Plotting
+  ggplot2::ggplot(df_total, aes(x = factor(eigengene), y = factor(phenotype))) +
+    geom_tile(fill = "white") +
+    geom_point(aes(colour = cor, size = signif)) +
+    scale_color_gradient2() +
+    theme_bw() +
+    xlab("Module") +
+    ylab("Phenotype")
+}
