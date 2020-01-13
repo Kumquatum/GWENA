19c7eabe2dddde63596ca39fd1b5ff7c2202d5e2
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2020-01-13 17:05:07 +0100

    correcting network_type parameters and consequences (bis)

--- R/GWENA.R
+++ R/GWENA.R
@@ -20,7 +20,7 @@
 GWENA_full <- function(data_expr,
                   # net_building
                   cor_func = c("pearson", "spearman", "bicor", "other"), your_func = NULL,
-                  power_value = NULL, fit_cut_off = 0.90, network_type = c("unsigned", "signed", "signed hybrid", "none"),
+                  power_value = NULL, fit_cut_off = 0.90, network_type = c("unsigned", "signed", "signed hybrid"),
                   tom_type = c("unsigned", "signed", "signed Nowick"), save_adjacency = FALSE,
                   # modules_detection
                   min_module_size = min(20, ncol(data_expr) / 2), merge_cut_height = 0.25,
