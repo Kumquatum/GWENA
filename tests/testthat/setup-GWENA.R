06418b10071e526db992c6f3791fc179d4ade5f3
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2020-02-03 14:45:38 +0100

    Comments

--- tests/testthat/setup-GWENA.R
+++ tests/testthat/setup-GWENA.R
@@ -1,9 +1,9 @@
-# library(dplyr)
+# ===== setup ====
 
 df_expr <- list(df_microarray = kuehne_expr[, 5000:7000],
                 df_rnaseq = gtex_expr[, 5000:7000])
 
-# ==== full_pipeline ====
+# ==== pipeline ====
 
 res_net <- build_net(df_expr$df_microarray)
 res_detection <- detect_modules(df_expr$df_microarray, res_net$network)
