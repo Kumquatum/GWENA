aa228773e6770c08c0588375b1c9f89079d19991
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2020-02-28 16:52:34 +0100

    First version comparison to test

--- R/comparison.R
+++ R/comparison.R
@@ -9,10 +9,11 @@
 #' @param cor_list list of matrices and/or data.frames, list of square tables representing correlation between
 #' each gene. Must be the same used to create networks in \code{\link{net_list}}. If NULL, will be re-calculated
 #' according to \code{cor_func}.
-#' @param ref string, condition name to be used as reference for permutation tests. Default will be "cross_comparison", meaning
-#' each condition will be tested as a reference.
-#' @param modules_list list of modules, list by condition of modules detected in each one. If only reference modules are provided,
-#' no contingency matrix will be computed.
+#' @param ref string, condition name to be used as reference for permutation tests or "cross comparison" if you want to compare
+#' each condition with the other as reference. Default will be the name of the first element
+#' in data_expr_list.
+#' @param modules_list list of modules or nested list of modules, list of modules in one condition (will be considered as the one from reference) or a condition
+#' named list with list of modules built in each one.
 #' @param cor_func string, name of the correlation function to be used. Must be one of "pearson", "spearman",
 #' "bicor", "other". If "other", your_func must be provided
 #' @param your_func function returning correlation values. Final values must be in [-1;1]
@@ -20,7 +21,7 @@
 #' @details
 #' \describe{
 #'   \item{To avoid recalculation, correlations matrices can be obtain by setting \code{keep_cor_mat} in \code{\link{net_list}} to TRUE.}
-#'   \item{Description of the 7 topological features used for preservation testing is available in \code{\link[NetRep]{modulePreservation}}}
+#'   \item{Description of the 7 topological features used for preservation testing is available in \code{\link[NetRep]{modulePreservation}}.}
 #' }
 #'
 #' @importFrom NetRep modulePreservation
@@ -28,28 +29,37 @@
 #'
 #' @export
 
-compare_modules = function(data_expr_list, net_list, cor_list = NULL, ref = "cross_comparison", modules_list,
+compare_modules = function(data_expr_list, net_list, cor_list = NULL, ref = names(data_expr_list)[1], modules_list,
                            cor_func = c("pearson", "spearman", "bicor", "other"), your_func = NULL,
-                           n_perm = 10000, comparison_modules = c("unpreserved", "preserved"), pvalue_th,
+                           n_perm = 10000, comparison_modules = c("unpreserved", "preserved", "one or the other"), pvalue_th,
                            n_threads, ...){
   # Checks
   if (!is.list(data_expr_list)) stop("data_expr_list must be a list.")
+  if (length(data_expr_list) < 2) stop("data_expr_list must have at least 2 elements (2 conditions) to run a comparison")
   if (!is.list(net_list)) stop("net_list must be a list.")
   if (!is.list(cor_list) && !is.null(cor_list)) stop("cor_list must be a list or NULL")
   if (!all(names(data_expr_list) %in% names(net_list))) stop("Names between data_expr_list and net_list don't correspond.")
   if (!all(names(data_expr_list) %in% names(cor_list))) stop("Names between data_expr_list and cor_list don't correspond.")
   lapply(data_expr_list, .check_data_expr)
   lapply(net_list, .check_network)
-  if (!is.character(ref)) stop("ref must be a string")
-  if (length(ref > 1)) stop("ref must be a single string")
-  if (ref != "cross_comparison" && !(ref %in% names(data_expr_list))) stop("ref must be a condition name, one of the names of data_expr_list elements.")
+  if (!is.character(ref)) stop("ref must be a string or a vector of strings")
+  if (ref != "cross comparison" && !(all(ref %in% names(data_expr_list)))) stop("ref must be a condition name, or a vector of it, matching names of data_expr_list.")
   if (!is.list(modules_list)) stop("modules_list must be a single list of modules or a list by condition of the modules")
-  if (all(names(modules_list) %in% names(data_expr_list))) { # Meaning it's a list by condition of modules
+  if (all(names(modules_list) %in% names(data_expr_list))) { # Meaning it's a list of modules by condition
     lapply(modules_list, function(cond){ .check_module(cond, is_list = TRUE) })
-    is_only_ref_modules = FALSE
+    is_modules_by_cond = TRUE
   } else { # Meaning it must be a single list of modules
     .check_module(modules_list, is_list = TRUE)
-    is_only_ref_modules = TRUE
+    is_modules_by_cond = FALSE
+  }
+  if (ref != "cross comparison") { # Checking adequation of content between conditions described in modules and reference choosen
+    if (!all(ref %in% names(modules_list))) stop("All conditions cited in ref must have their modules described in modules_list")
+    if (!all(names(modules_list) %in% ref)){
+      warning("More conditions were defined in modules_list than those cited in ref. Keeping only the matching ones")
+      modules_list <- modules_list[ref]
+    }
+  } else {
+      if (!all(names(data_expr_list) %in% names(modules_list))) stop("All conditions in data_expr_list must have their modules described in modules_list")
   }
   cor_func <- match.arg(cor_func)
   if (cor_func == "other" && (is.null(your_func) || !is.function(your_func))) stop("If you specify other, your_func must be a function.")
@@ -78,29 +88,25 @@ compare_modules = function(data_expr_list, net_list, cor_list = NULL, ref = "cro
 
   # Params preparation
   conditions <- names(data_expr_list)
-
-  if (is_only_ref_modules) {
-    # ref must be a single condition (because modulePreservation require to have the same conditions passed to moduleAssignements and discovery)
-    if (length(ref) > 1) {
-      warning("Modules given for multiple condition, keeping only the one matching ref provided")
-      modules_list <- modules_list[[ref]]
-    }
-    modules_reformated <- lapply(names(modules_list), function(x){
-      setNames(rep(as.numeric(x), length(modules_list[[x]])), modules_list[[x]]) }) %>% unlist
-  } else {
-    # modules_reformated must be formated like single condition but for each condition, with the same list names
+  if (is_modules_by_cond) {
+    # list by condition of gene named vector of modules values
     modules_reformated <- sapply(modules_list, function(cond){
       lapply(names(cond), function(x){
         setNames(rep(as.numeric(x), length(cond[[x]])), cond[[x]]) }) %>% unlist
     }, simplify = FALSE)
+  } else {
+    # Single gene named vector of modules values
+    modules_reformated <- lapply(names(modules_list), function(x){
+      setNames(rep(as.numeric(x), length(modules_list[[x]])), modules_list[[x]]) }) %>% unlist
   }
-
-  test_tail_side <- ifelse(comparison_modules != "unpreserved", "greater", "lesser")
-  if (ref == "cross_comparison") {
+  test_tail_side <- switch(comparison_modules,
+                           "unpreserved" = "less",
+                           "preserved" = "greater",
+                           "one or the other" = "two.sided"
+                           )
+  if (ref == "cross comparison") {
     test_set <- conditions
     ref_set <- conditions
-    # Must check that discovery names are the same as moduleAssignments
-    # if () stop("")
   } else {
     test_set <- conditions[which(conditions != ref)]
     ref_set <- ref
@@ -114,35 +120,33 @@ compare_modules = function(data_expr_list, net_list, cor_list = NULL, ref = "cro
     simplify = FALSE, ...))
 
 
-  # modulePreservation doesn't compute contingency matrix when using only one discovery set, so adding it
-  if (ref != "cross_comparison") {
+  # modulePreservation doesn't compute contingency matrix when single ref, so adding it
+  if (ref != "cross comparison" && length(ref) == 1) {
     lapply(test_set, function(cond){
       preservation[[ref]][[cond]][["contingency"]] <- NetRep:::contingencyTable(modules_reformated,
                                                                                 modules_reformated[[ref]] %>% table %>% names,
                                                                                 modules_reformated[[cond]] %>% names)
     })
-
   }
 
-  # Formating depending on number of conditions tested
-  if (length(test_set > 1)) {
+  preservation_augmented <- lapply(preservation, function(ref_set){
+    lapply(ref_set, function(test_set){
+      modules_of_interest <- preservation[[ref_set]][[test_set]][["p.values"]] %>%
+        apply(1, function(mod_stats){
+          if (all(mod_stats < pvalue_th/2)) { "unpreserved"
+          } else if (all(mod_stats > (1 - pvalue_th/2))) { "preserved"
+          } else { "" }
+        }) %>% data.frame(module = names(.), comparison = .)
 
-  } else {
-
-  }
-
-
-  # Modules preserved (those which p value < 0.01)
-  max_pval <- apply(preservation$p.value, 1, max)
-  preserved_modules = which(max_pval < pvalue_th)
-
-  ## Looking for statistics involved into non concervation of the modules
-  unpreserved_modules = which(max_pval > pvalue_th)
-  statsImplied = apply(as.data.frame(unpreserved_modules), 1, function(x){
-    res = names(x)[which(x>0.01)]
+      list(modules_of_interest = modules_of_interest,
+           detail = test_set)
+    })
   })
 
-
-
-
+  # Simplifying final object (yes, modulePreservation has one but I need to get the none simplified version to add contingency matrix correctly)
+  prune_list <- function(x) {
+    x <- lapply(x, function(y) if (is.list(y)) prune_list(y) else y)
+    purrr::compact(x)
+  }
+  preservation_augmented <- prune_list(preservation_augmented)
 }
