7c190486567a64d203e748245ccac772718964af
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2020-03-10 14:37:05 +0100

    Correcting handling of case when gprofiler could be down

--- tests/testthat/setup-GWENA.R
+++ tests/testthat/setup-GWENA.R
@@ -4,12 +4,13 @@ df_expr <- list(df_microarray = kuehne_expr[, 5000:7000],
                 df_rnaseq = gtex_expr[, 5000:7000])
 
 # Checking if gprofiler is up. If not, skipping tests.
-# Note : couldn't check API for gost itself since no GET endpoint with pin method set by authors
+# Note : couldn't check properly API for gost since no GET method set by authors, so
+#        checking through a GET query to the POST method and using error code as check.
 safe_get <- purrr::safely(httr::GET)
 i <- 1
 while (i <= 4) {
-  res_get <- safe_get("https://biit.cs.ut.ee/gprofiler/", httr::timeout(i*10))
-  if (!is.null(res_get$result)) { i <- 3 } else { i <- i + 1 }
+  res_get <- safe_get("https://biit.cs.ut.ee/gprofiler/api/gost/profile/", httr::timeout(i*10))
+  if (!is.null(res_get$result)) { i <- 5 } else { i <- i + 1 }
 }
 if (!is.null(res_get$result)) {is_gprofiler_down <- FALSE } else { is_gprofiler_down <- TRUE }
 
