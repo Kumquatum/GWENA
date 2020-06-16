9b9d6328b20a659a05034622151bd054211dbd7c
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2020-06-16 14:06:46 +0200

    Correcting tests according to build_net changes

--- tests/testthat/setup-GWENA.R
+++ tests/testthat/setup-GWENA.R
@@ -15,15 +15,18 @@ S4Vectors::metadata(se) <- list(
 )
 
 #### Checking if gprofiler is up. If not, skipping tests.
-# Note : couldn't check properly API for gost since no GET method set by authors, so
-#        checking through a GET query to the POST method and using error code as check.
+# Note : couldn't check properly API for gost since no GET method set by
+# authors, so checking through a GET query to the POST method and using error
+# code as check.
 safe_get <- purrr::safely(httr::GET)
 i <- 1
 while (i <= 4) {
-  res_get <- safe_get("https://biit.cs.ut.ee/gprofiler/api/gost/profile/", httr::timeout(i*10))
+  res_get <- safe_get("https://biit.cs.ut.ee/gprofiler/api/gost/profile/",
+                      httr::timeout(i*10))
   if (!is.null(res_get$result)) { i <- 5 } else { i <- i + 1 }
 }
-if (!is.null(res_get$result)) {is_gprofiler_down <- FALSE } else { is_gprofiler_down <- TRUE }
+if (!is.null(res_get$result)) {is_gprofiler_down <- FALSE
+} else { is_gprofiler_down <- TRUE }
 
 
 # ==== pipeline steps needed to avoid redundant operations ====
